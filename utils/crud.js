93ef2c1f9b68ae6b6e68266a7407ea47b9b43c70
milo2112 <mjrp.ros@gmail.com>
2023-09-30 13:18:42 -0300

    Adding delete Feature

--- utils/crud.js
+++ utils/crud.js
@@ -10,27 +10,50 @@ const setSongs = (songs) => {
 
 const createSong = (song) => {
   const songs = getSongs()
+  // console.log(`antes del push -> ${songs}`)
+  // console.log(songs.id)
+  const { cancion } = song
+  // console.log(`\nid-->${id} - is type of: ${typeof id}\n`)
+  console.log(cancion)
   songs.push(song)
+  // console.log(`despues del push -> ${songs}`)
+  // console.log(songs)
   setSongs(songs)
   return 'Canción agregada...'
 }
 
 const readSongs = () => getSongs()
 
-const updateSong = (id, modifySong) => {
-  id = Number(id)
+const updateSong = (id, modifiedSong) => {
+  // id = Number(id)
+  console.log(id)
   const retrievedSongs = getSongs()
   console.log(retrievedSongs)
+  console.log('INICIO->modifiedSong')
+  console.log(modifiedSong)
+  console.log('FIN->modifiedSong\n')
   const indexSong = retrievedSongs.findIndex((song) => song.id === id)
   retrievedSongs[indexSong] = {
     id,
-    ...modifySong
+    ...modifiedSong
   }
+  console.log('INICIO->retrievedSongs')
+  console.log(retrievedSongs)
+  console.log('FIN->retrievedSongs\n')
   setSongs(retrievedSongs)
 }
 
+const deleteSong = (id) => {
+  id = Number(id)
+  const retrievedSongs = getSongs()
+  // console.log(retrievedSongs)
+  const indexSong = retrievedSongs.findIndex((song) => song.id === id)
+  retrievedSongs.splice(indexSong, 1)
+  setSongs(retrievedSongs)
+}
 module.exports = {
   readSongs,
   createSong,
-  updateSong
+  updateSong,
+  deleteSong
 }
